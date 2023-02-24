5e89495d69d9cadb31488f9bcc1305a889b92af6
AMwa1 <98977562+AMwa1@users.noreply.github.com>
2023-02-23 21:12:46 -0500

    Populate login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,21 @@
+// Thank you to https://www.section.io/engineering-education/keyboard-events-in-javascript/ for the keyboard event.
+document.addEventListener("keydown", (event) => 
+{
+    if (event.key == "Enter")
+    {
+        login();
+    }
+}, false);
 
+function login()
+{
+    if (document.getElementById("usernameIn").value == "an" && document.getElementById("passwordIn").value == "mo")
+    {
+        document.getElementById("wrong").innerText = "Actually not wrong!";
+        window.open("game.html");
+    }
+    else
+    {
+        document.getElementById("wrong").innerText = "Incorrect username or password.";
+    }
+}
